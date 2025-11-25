fe8e823922c5f0e57c192d26e13e0e28c3a99448
jovana00726 <jovanacen21@gmail.com>
2025-11-25 12:38:17 +0700

    Implement factorial and sum functions in p1.cpp

--- src/p1.cpp
+++ src/p1.cpp
@@ -1,10 +1,23 @@
-// 題目 1：高塔建築師 (The Tower Architect) - 20分
-
 #include <iostream>
 using namespace std;
-
-int main() {
-    
-    
+long long factorial (int n){
+    if (n == 0 || n==1){
+        return 1;
+    } else{
+        return n*factorial(n-1);
+    }
+}
+long long sum (int n){
+    if (n==0 || n==1){
+        return 1;
+    }else {
+        return n+sum(n-1);
+    }
+}
+int main (){
+    int n;
+    cin >> n;
+    cout << factorial (n)<< endl;
+    cout << sum(n) << endl;
     return 0;
 }
